02becb9b355077dbede461f010245ea94e997410
Pyro <oreinavallecillo@gmail.com>
2023-06-05 14:24:32 -0600

    Practica 1 , Pagina Web de Wiki sobre el LOL

---
+++ src/main/java/com/EA_08_Practicas_2/Ea08Practicas2Application.java
@@ -0,0 +1,13 @@
+package com.EA_08_Practicas_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ea08Practicas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ea08Practicas2Application.class, args);
+	}
+
+}
